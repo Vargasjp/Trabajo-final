98c031ab8585606805e62415c11f45b73effbe8e
Leonardo Palacios <leonardoariel.palacios94@gmail.com>
2022-09-22 23:19:05 -0300

    falta colocar listado en destinatario de mensajes

--- AppMensajeria/forms.py
+++ AppMensajeria/forms.py
@@ -1,12 +1,15 @@
+from collections import UserList
 from django import forms
 from ckeditor.fields import RichTextFormField
+
+from AppConcesionaria.forms import UserRegisterForm
 
 
 class FormularioMensaje(forms.Form):
 
     nombre=forms.CharField(max_length=30)
     apellido=forms.CharField(max_length=30)
-    destinatario=forms.CharField(max_length=40)
+    destinatario=forms.ChoiceField(choices=UserRegisterForm, required=True, label="Seleccione Usuario destinatario")
     email=forms.EmailField(max_length=50)
     mensaje=RichTextFormField()
     fecha=forms.DateField()
--- AppMensajeria/models.py
+++ AppMensajeria/models.py
@@ -9,7 +9,7 @@ class Mensaje(models.Model):
     fecha=models.DateField()
     nombre=models.CharField(max_length=30)
     apellido=models.CharField(max_length=30)
-    destinatario=models.ForeignKey(User, on_delete=models.CASCADE, related_name="destinatario")
+    destinatario=models.ForeignKey(User, related_name="destinatario", on_delete=models.CASCADE)
     email=models.EmailField(max_length=50)
     mensaje=RichTextField()
     fecha=models.DateTimeField()
